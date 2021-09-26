3f7c67b51d5fa09ddc571169cd18b86d244dd69e
4lch4 <git@4lch4.email>
2021-09-26 17:41:25 -0500

    feat: updated sample

--- sample/index.ts
+++ sample/index.ts
@@ -1,12 +1,12 @@
+import { ClientErrors, Successful } from '@4lch4/koa-oto'
 import Router from '@koa/router'
 import Koa from 'koa'
 
 const app = new Koa()
 const router = new Router()
 
-router.get('/', async ctx => {
-  ctx.body = 'Hello World'
-})
+router.get('/', async ctx => Successful.ok(ctx, 'Hello World'))
+router.get('/error', ctx => ClientErrors.badRequest(ctx, 'Bad Request'))
 
 app.use(router.routes())
 app.use(router.allowedMethods())
