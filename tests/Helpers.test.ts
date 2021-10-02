b86371e5123d98e470f330d382553ce1114928d9
4lch4 <git@4lch4.email>
2021-10-02 15:02:57 -0500

    feat: added a test for Helpers.handleHeaders

--- tests/Helpers.test.ts
+++ tests/Helpers.test.ts
@@ -1,9 +1,11 @@
+import Axios from 'axios'
 import * as chai from 'chai'
 import * as mocha from 'mocha'
 import { Helpers } from '../'
 
 const { expect } = chai
 const { it, describe } = mocha
+const axios = Axios.create({ baseURL: 'http://localhost:3240/test' })
 
 describe('Basic validation of exported Helpers class', () => {
   it('should export a function called isArgs', () => {
@@ -15,5 +17,18 @@ describe('Basic validation of exported Helpers class', () => {
     expect(Helpers.isArgs({ body: 'Hello' })).to.be.true
     expect(Helpers.isArgs({ msg: 'Hello' })).to.be.true
     expect(Helpers.isArgs({ headers: { key: 'value' } })).to.be.true
+  })
+
+  it('should handle headers for return values', async () => {
+    try {
+      const headers = {
+        'Content-Type': 'application/json',
+        key: 'value'
+      }
+      await axios.get('/classHelpers', { headers })
+    } catch (error) {
+      console.error(error)
+      return Promise.reject(error)
+    }
   })
 })
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,12 @@
 import Router from '@koa/router'
+import * as chai from 'chai'
 import { Server } from 'http'
-import Koa from 'koa'
+import Koa, { ParameterizedContext } from 'koa'
 import * as mocha from 'mocha'
+import { Helpers } from '../src'
 import { AllRoutes } from './TestRouteData'
 
+const { expect } = chai
 const { before, after } = mocha
 const TestConfig = {
   port: 3240,
@@ -22,9 +25,18 @@ before(() => {
       return { key, path: `/${key}` }
     })
     .forEach(({ key, path }) => {
-      console.log(`Mapping ${key} to ${path} path...`)
+      // console.log(`Mapping ${key} to ${path} path...`)
       router.get(path, AllRoutes[key])
     })
+
+  router.get('/classHelpers', async (ctx: ParameterizedContext) => {
+    Helpers.handleHeaders(ctx, {
+      headers: { key: 'value' }
+    })
+
+    ctx.status = 200
+    expect(ctx.get('key')).to.equal('value')
+  })
 
   app.use(router.routes())
   app.use(router.allowedMethods())
